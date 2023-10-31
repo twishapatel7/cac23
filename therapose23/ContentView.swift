4b58c3f3617dc19dba96d4ee2558906b01f289f3
twishapatel7 <twishapatel2007@gmail.com>
2023-10-30 21:53:39 -0400

    Initial Commit

---
+++ therapose23/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  therapose23
+//
+//  Created by Twisha Patel on 10/30/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ therapose23/therapose23App.swift
@@ -0,0 +1,17 @@
+//
+//  therapose23App.swift
+//  therapose23
+//
+//  Created by Twisha Patel on 10/30/23.
+//
+
+import SwiftUI
+
+@main
+struct therapose23App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
